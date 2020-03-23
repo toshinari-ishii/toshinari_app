f14f9ab7d28b241f4f2b36c2633a5de6f9ee14c7
toshinari-ishii <toshi1225m@gmail.com>
2020-03-23 16:46:56 +0900

    day-3-1

---
+++ app/assets/javascripts/channels/tops.js
@@ -0,0 +1,8 @@
+$(function () {
+  setTimeout(function () {
+    $('.start p').fadeIn(4000);
+  }, 500); //0.5秒後にロゴをフェードイン!
+  setTimeout(function () {
+    $('.start').fadeOut(2000);
+  }, 4500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
